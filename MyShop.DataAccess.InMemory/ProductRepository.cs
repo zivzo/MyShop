1ccf2f8ed7e6169a4fb6b7d11a32749cdd30841b
zivzo <zivzorero16@gmail.com>
2022-02-01 10:13:36 +0200

    added new solution files

---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+namespace MyShop.DataAccess.InMemory
+{
+    internal class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        public void Update(Product p)
+        {
+            Product product = products.Find(x => x.Id == p.Id);
+            if (product != null)
+            {
+                product = p;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+        public Product Find(string id) {
+            Product product = products.Find(x => x.Id == d);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+       public IQueryable<Product> Collection()
+        {
+                return products.AsQueryable();  
+        }
+        public void Delete(string id)
+        {
+             Product pro = products.Find(x => x.Id == id);
+            if (pro != null)
+            {
+                products.Remove(pro);
+            }
+            else
+            {
+                throw new Exception("no product found");
+            }
+        }
+    }
+}
