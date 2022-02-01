72cbdd838510af9363396b9bf2e9620da2d923f5
zivzo <zivzorero16@gmail.com>
2022-02-01 15:12:47 +0200

    added a prudctcategory viewModel

---
+++ MyShop.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+namespace MyShop.Core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+        public Product product { get; set; }
+        public IEnumerable<ProductCategory> productCategories { get; set; }
+    }
+}
